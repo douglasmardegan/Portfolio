4aa6695991eb6a922d6cf9952f3333691cc3d47c
Douglas Mardegan Colodete <douglasmardegan@yahoo.com.br>
2022-03-01 13:40:48 -0300

    Upgrade project

--- freelancer_numberGenerator/script.js
+++ freelancer_numberGenerator/script.js
@@ -1,23 +1,24 @@
+let generateContentBtn = document.querySelector("#generateContentBtn");
+let clearContentBtn = document.querySelector("#clearContentBtn");
+
 let arrayIds = [];
 let ids = document.getElementsByClassName("grid-item");
 for (let i = 0; i < ids.length; i++) {
   arrayIds.push(ids[i].id);
 }
 
-document
-  .querySelector("#generateContentButton")
-  .addEventListener("click", () => {
-    /* Função para sortear um número aleatório entre 0 e 99 */
-    function getRandomInt(min, max) {
-      return Math.floor(Math.random() * (max - min + 1) + min);
-    }
+generateContentBtn.addEventListener("click", () => {
+  /* Função para sortear um número aleatório entre 0 e 99 */
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
-    for (let i = 0; i < arrayIds.length; i++) {
-      document.getElementById(arrayIds[i]).innerHTML = `${getRandomInt(0, 99)}`;
-    }
-  });
+  for (let i = 0; i < arrayIds.length; i++) {
+    document.getElementById(arrayIds[i]).innerHTML = `${getRandomInt(0, 99)}`;
+  }
+});
 
-document.querySelector("#clearContentButton").addEventListener("click", () => {
+clearContentBtn.addEventListener("click", () => {
   for (let i = 0; i < arrayIds.length; i++) {
     document.getElementById(arrayIds[i]).innerHTML = "";
   }
